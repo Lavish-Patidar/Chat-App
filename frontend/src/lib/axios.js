52ff5cdf77259237adaa4fca808a3b09b981bc1c
Lavish Patidar <lavishpatidar9926@gmail.com>
2025-03-21 17:03:34 +0530

    Vercel Confige 3

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chat-app-oklf.vercel.app/api" : "https://chat-app-sooty-psi.vercel.app/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chat-app-oklf.vercel.app/api" : "/api",
   withCredentials: true,
 });
