4ff6a36ae746cd8389ddb4cf6427168eb8c66784
Lavish Patidar <lavishpatidar9926@gmail.com>
2025-03-24 09:50:02 +0530

    vercel confige 1

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,18 +11,19 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
-app.use(cookieParser());
 app.use(
   cors({
     origin: "https://chat-app-zeta-woad-50.vercel.app",
     credentials: true,
   })
 );
+app.use(express.json());
+
+app.use(cookieParser());
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
