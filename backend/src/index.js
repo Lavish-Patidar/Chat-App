da9fc1961149ece5f2185179b527b8b2e50ade28
Lavish Patidar <lavishpatidar9926@gmail.com>
2025-03-24 10:31:35 +0530

    vercel confige 6

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors"; // Import CORS
 import connectDB from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -9,6 +10,13 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
+
+app.use(
+  cors({
+    origin: "https://chat-app-zeta-woad-50.vercel.app",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
