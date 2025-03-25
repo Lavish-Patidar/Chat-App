ce77b450765b17274ee39f6dd2124806789031fb
Lavish Patidar <lavishpatidar9926@gmail.com>
2025-03-25 10:02:57 +0530

    render try 1

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chat-app-api-mu-gules.vercel.app/api",
+  baseURL: "https://chat-app-api-z27f.onrender.com/api",
   withCredentials: true,
 });
